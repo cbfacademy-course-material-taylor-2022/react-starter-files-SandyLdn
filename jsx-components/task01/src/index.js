0da71ec6b30460a81c36794f9483a11263e235df
San <san@Sans-MacBook-Air.local>
2022-11-11 00:02:19 +0000

    starter react task

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const name = 'Sandra';
 
 function Profile(name){
+  if (name !== ''){
     return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
+              <p>{name}'s Example of react JSX in action!!</p>
             </main>
+} else {
+  return <main id="content" role="main" className="base">
+            <h1>Just a React Page</h1>
+              <p>Nothing to see here!</p>
+            </main>
+}
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile(name));
 
