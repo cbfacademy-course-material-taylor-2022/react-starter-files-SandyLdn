4fcd76e6e6bfe2dda088024a89a570b3e9cc7b88
San <san@Sans-MacBook-Air.local>
2022-11-15 20:40:00 +0000

    git bookcase data display titles in book component

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './components/Book';
 import data from './models/books.json';
 
 function App() {
-  const books = data;
-  return <Book/>;
+  //const state = useState(data);
+  //const books = state[0]
+  const [books] = useState(data)
+  console.log (books)
+  return books.map(book => <Book key={book.id} book={book}/>);;
 }
 
 export default App;
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Book() {
-  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
+
+const Book = (props) => {
+  return (
+   <div>
+     {/* Code below displays the book title */}
+     <h2>{props.book.volumeInfo.title}</h2>
+     {
+       /*
+       Add other JSX elements that will return:
+       -Author Name
+       -Thumbnail Image
+       -Book Price 
+       */
+     }
+   </div>
+  );
 }
 
 export default Book;
